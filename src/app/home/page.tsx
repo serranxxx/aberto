3cd2a3a59bf1d078acdb7496f0a9bc6c341f0843
serranxxx <albserrano8@gmail.com>
2026-03-17 16:30:32 -0600

    issues

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
       <div className={styles.content}>
         <span className={styles.key_title}>Luis Alberto Serrano</span>
 
-        <span className={styles.key_sub}>Product designer & Developer</span>
+        <span className={styles.key_sub_title}>Product designer & Developer</span>
 
         <span className={styles.single_text}>
           I create digital products where <b>technology, design, and user experience work in harmony.</b> My approach combines the structured thinking of engineering with the creativity of design to build software with intention and purpose.
